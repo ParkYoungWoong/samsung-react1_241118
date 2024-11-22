6e7e3035c379167e3db452ef48a86ae3d106f835
ParkYoungWoong <thesecon@gmail.com>
2024-11-22 17:51:01 +0900

    node-fetch를 axios로 교체.

--- api/todos.ts
+++ api/todos.ts
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+import axios from 'axios'
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 
 interface RequestBody {
@@ -11,19 +11,16 @@ export default async function (req: VercelRequest, res: VercelResponse) {
     endpoint = '',
     method = 'GET',
     data
-  } = (JSON.parse(req.body || '{}') || {}) as Partial<RequestBody>
-  const response = await fetch(
-    `https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos/${endpoint}`,
-    {
-      method,
-      headers: {
-        'content-type': 'application/json',
-        apikey: process.env.TODO_APIKEY || '',
-        username: process.env.TODO_USERNAME || ''
-      },
-      body: JSON.stringify(data)
-    }
-  )
-  const json = await response.json()
-  res.status(200).json(json)
+  } = (req.body || {}) as Partial<RequestBody>
+  const { data: responseValue } = await axios({
+    url: `https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos/${endpoint}`,
+    method,
+    headers: {
+      'content-type': 'application/json',
+      apikey: process.env.TODO_APIKEY,
+      username: process.env.TODO_USERNAME
+    },
+    data
+  })
+  res.status(200).json(responseValue)
 }
